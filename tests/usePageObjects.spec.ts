30ee3d2f9dc2065b3cfa41ce626bfc2cda17069d
Yevhen Tarasenko <yevhen.tarasenko1995@gmail.com>
2025-06-28 11:44:42 +0300

    POM: Lesson 49. Parametrized Methods

--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -1,11 +1,12 @@
-import {test, expect} from '@playwright/test';
-import {NavigationPage} from '../src/pages/NavigationPage';
+import { test, expect } from '@playwright/test';
+import { NavigationPage } from '../src/pages/NavigationPage';
+import { FormLayoutPage } from '../src/pages/FormLayoutsPage';
 
-test.beforeEach(async ({page}) => {
+test.beforeEach(async ({ page }) => {
     await page.goto('http://localhost:4200/');
 });
 
-test('navigate to form page', async ({page}) => {
+test('navigate to form page', async ({ page }) => {
     const navigateTo = new NavigationPage(page);
 
     await navigateTo.formLayoutsPage();
@@ -13,4 +14,17 @@ test('navigate to form page', async ({page}) => {
     await navigateTo.smartTablePage();
     await navigateTo.toastrPage();
     await navigateTo.tooltipPage();
+});
+
+test('parametrized methods', async ({ page }) => {
+    const navigateTo = new NavigationPage(page);
+    const onFormLayoutPage = new FormLayoutPage(page);
+
+    await navigateTo.formLayoutsPage();
+    await onFormLayoutPage.submitUsingTheGridFormWithCredentialsAndSelectOption(
+        'test@gmail.com',
+        'Qwerty12345%',
+        'Option 1',
+    );
+    await onFormLayoutPage.submitInlineFormWithNameEmailAndCheckbox('Yevhen', 'test1@gmail.com', true);
 });
