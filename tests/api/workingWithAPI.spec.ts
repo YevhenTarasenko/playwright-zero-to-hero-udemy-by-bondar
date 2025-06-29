d41d6e8c5329da6672e1ceeff6a797c3354caef8
Yevhen Tarasenko <yevhen.tarasenko1995@gmail.com>
2025-06-29 16:41:17 +0300

    API: Lesson 55. Mocking API

--- tests/api/workingWithAPI.spec.ts
+++ tests/api/workingWithAPI.spec.ts
@@ -1,9 +1,17 @@
-import test, { expect } from '@playwright/test';
+import test, { expect } from "@playwright/test";
+import tags from "../../src/test-data/tags.json";
 
 test.beforeEach(async ({ page }) => {
-    await page.goto('https://conduit.bondaracademy.com/');
+    await page.route("*/**/api/tags", async (route) => {
+        await route.fulfill({
+            body: JSON.stringify(tags),
+        });
+    });
+
+    await page.goto("https://conduit.bondaracademy.com/");
+    await page.waitForResponse("https://conduit-api.bondaracademy.com/api/tags");
 });
 
-test('check', async ({ page }) => {
-    await expect(page.locator('.navbar-brand')).toHaveText('conduit');
+test("check", async ({ page }) => {
+    await expect(page.locator(".navbar-brand")).toHaveText("conduit");
 });
