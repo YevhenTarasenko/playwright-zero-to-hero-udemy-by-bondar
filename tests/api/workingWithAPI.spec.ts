3c2679a40b53b2124d377662d46e6514bcfa470c
Yevhen Tarasenko <yevhen.tarasenko1995@gmail.com>
2025-07-01 19:45:39 +0300

    API: Lesson 58. Intercept Browser API Response

--- tests/api/workingWithAPI.spec.ts
+++ tests/api/workingWithAPI.spec.ts
@@ -87,3 +87,43 @@ test("create article from list", async ({ page, request }) => {
     // await page.getByText("Global Feed").click();
     // await expect(page.locator("app-article-list h1").first()).not.toContainText("PW - Title");
 });
+
+test("create an article and delete", async ({ page, request }) => {
+    //create an article
+    await page.getByText("New Article").click();
+    await page.getByRole("textbox", { name: "Article Title" }).fill("PW is awesome");
+    await page.getByRole("textbox", { name: "What's this article about?" }).fill("PW this is article about");
+    await page
+        .getByRole("textbox", { name: "Write your article (in markdown)" })
+        .fill("We like to use PW for automation");
+    await page.getByRole("button", { name: "Publish Article" }).click();
+
+    //get slugID
+    const articleResponse = await page.waitForResponse("https://conduit-api.bondaracademy.com/api/articles/");
+    const articleResponseBody = await articleResponse.json();
+    const slugID = articleResponseBody.article.slug;
+    await expect(page.locator("app-article-page h1")).toContainText("PW is awesome");
+
+    //check the article on Global Feed
+    await page.getByText("Home").click();
+    await page.getByText("Global Feed").click();
+    await expect(page.locator("app-article-list h1").first()).toContainText("PW is awesome");
+
+    //get accessToken
+    const response = await request.post("https://conduit-api.bondaracademy.com/api/users/login", {
+        data: {
+            user: {
+                email: "yevhentarasenko@gmail.com",
+                password: "Qwerty12345%",
+            },
+        },
+    });
+    const responseBody = await response.json();
+    const accessToken = responseBody.user.token;
+
+    //delete article
+    const deleteArticleResponse = await request.delete(`https://conduit-api.bondaracademy.com/api/articles/${slugID}`, {
+        headers: { authorization: `Token ${accessToken}` },
+    });
+    expect(deleteArticleResponse.status()).toEqual(204);
+});
