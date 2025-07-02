952998d1f1a24a77732dd28e612cfc04bbd5f55e
Yevhen Tarasenko <yevhen.tarasenko1995@gmail.com>
2025-07-02 20:14:30 +0300

    Advanced: Lesson 62. Test Data Generatior

--- tests/web/usePageObjects.spec.ts
+++ tests/web/usePageObjects.spec.ts
@@ -1,11 +1,12 @@
-import { test } from '@playwright/test';
-import { PageManager } from '../../src/pages/PageManager';
+import { test } from "@playwright/test";
+import { PageManager } from "../../src/pages/PageManager";
+import { faker } from "@faker-js/faker";
 
 test.beforeEach(async ({ page }) => {
-    await page.goto('http://localhost:4200/');
+    await page.goto("http://localhost:4200/");
 });
 
-test('navigate to form page', async ({ page }) => {
+test("navigate to form page", async ({ page }) => {
     const pm = new PageManager(page);
 
     await pm.navigateTo().formLayoutsPage();
@@ -15,14 +16,16 @@ test('navigate to form page', async ({ page }) => {
     await pm.navigateTo().tooltipPage();
 });
 
-test('parametrized methods', async ({ page }) => {
+test("parametrized methods", async ({ page }) => {
     const pm = new PageManager(page);
+    const randomFullName = faker.person.fullName();
+    const randomEmail = `${randomFullName.replace(" ", "")}${faker.number.int(1000)}@test.com`;
 
     await pm.navigateTo().formLayoutsPage();
     await pm
         .onFormLayoutPage()
-        .submitUsingTheGridFormWithCredentialsAndSelectOption('test@gmail.com', 'Qwerty12345%', 'Option 1');
-    await pm.onFormLayoutPage().submitInlineFormWithNameEmailAndCheckbox('Yevhen', 'test1@gmail.com', true);
+        .submitUsingTheGridFormWithCredentialsAndSelectOption("test@gmail.com", "Qwerty12345%", "Option 1");
+    await pm.onFormLayoutPage().submitInlineFormWithNameEmailAndCheckbox(randomFullName, randomEmail, true);
     await pm.navigateTo().datepickerPage();
     await pm.onDatePickerPage().selectCommonDatePickerDateFromToday(5);
     await pm.onDatePickerPage().selectDatePickerWithRangeFromToday(6, 15);
