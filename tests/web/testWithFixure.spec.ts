9748cc0dff77ac5586f330b83c35b7ab936cace5
Yevhen Tarasenko <yevhen.tarasenko1995@gmail.com>
2025-07-09 19:53:04 +0300

    Advanced: Lesson 68. Fixtures

---
+++ tests/web/testWithFixure.spec.ts
@@ -0,0 +1,16 @@
+import { test } from "../../test-options";
+import { faker } from "@faker-js/faker";
+
+test("parametrized methods", async ({ pageManager }) => {
+    const randomFullName = faker.person.fullName();
+    const randomEmail = `${randomFullName.replace(" ", "")}${faker.number.int(1000)}@test.com`;
+
+    await pageManager
+        .onFormLayoutPage()
+        .submitUsingTheGridFormWithCredentialsAndSelectOption(
+            process.env.USER_NAME!,
+            process.env.PASSWORD!,
+            "Option 1",
+        );
+    await pageManager.onFormLayoutPage().submitInlineFormWithNameEmailAndCheckbox(randomFullName, randomEmail, true);
+});
